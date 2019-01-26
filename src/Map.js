ee219519fa095083f0a359b4e2d11b77c05ddb28
Serj <sgtserj94@gmail.com>
2019-01-26 14:09:06 +0900

    asd
    asd

---
+++ src/Map.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+
+class Map extends Component {
+  state = {
+    hex_map: [[1,1,1],[1,1,1,1,1,1,1],[1,1,1],[1,1,1],[1,1,1,1,1,1],[1,1,1],[1,1,1],]
+  }
+
+  oneHex = (index) => {
+    return (
+      <div>
+        {this.state.hex_map[index].map((row, lol) => {
+          return (      
+            <div class="hexa">
+              <div className="hex1">
+                <div className="hex2" style={{backgroundColor: "red"}}>
+                  {index},{lol}
+                </div>
+              </div>
+            </div>
+          )
+        } )}
+      </div>
+    )
+  }
+
+renderHexRow = () => {
+
+}
+
+  renderHexMap = () => {
+    return (
+      <div className="hex-map-row-super-container">
+        {this.state.hex_map.map((row, index) => {
+          let padding = () => {
+            if (index%2 == 0) {
+              return {paddingTop: "115px"}
+            } else {
+              return {paddingTop: "0px"}
+            }
+ 
+          }
+            return (
+              <div className="hex-container-row" style={padding()}>
+                {this.oneHex(index)}
+              </div>
+              )
+        })}
+      </div>
+    )
+  }
+
+
+  render() {
+    return (
+        <div>
+            {this.renderHexMap()}
+        </div>
+    )
+  }
+}
+
+export default Map;
